551d81e815c18fc68b6db4f0e4c2311fad712d53
Fr?d?ric Gillioz <fredgillioz@web.de>
2017-07-07 22:24:56 +0200

    * WIP IfcSectionedSolidHorizontal

---
+++ Infrastructure/src/OpenInfraPlatform/Infrastructure/SlabField/SlabFieldElement.h
@@ -0,0 +1,59 @@
+/*
+    Copyright (c) 2017 Technical University of Munich
+    Chair of Computational Modeling and Simulation.
+
+    TUM Open Infra Platform is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 3
+    as published by the Free Software Foundation.
+
+    TUM Open Infra Platform is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __SLABFIELDELEMENT_H_4E0E43F3630D4764A9CD2FA6214EF8A6__
+#define __SLABFIELDELEMENT_H_4E0E43F3630D4764A9CD2FA6214EF8A6__
+
+#include <OpenInfraPlatform/Infrastructure/namespace.h>
+#include <OpenInfraPlatform/Infrastructure/OIPInfrastructure.h>
+#include <OpenInfraPlatform/Infrastructure/SectionedSolid/SectionedSolidHorizontal.h>
+#include <BlueFramework/Core/string.h>
+
+#include <vector>
+
+OIP_NAMESPACE_OPENINFRAPLATFORM_INFRASTRUCTURE_BEGIN
+
+class BLUEINFRASTRUCTURE_API SlabFieldElement
+{
+public:
+	typedef std::vector<SectionedSolidHorizontal> SectionedSolidVector;
+
+	buw::String getName() const;
+	void setName(std::wstring const& name);
+
+	SectionedSolidVector const& getSectionedSolids() const;
+	void addSectionedSolid(SectionedSolidHorizontal const& ssh);
+	void addSectionedSolid(SectionedSolidVector const& sshs);
+
+protected:
+	explicit SlabFieldElement(int const id, std::wstring const& name);
+	virtual ~SlabFieldElement();
+
+private:
+	int const id;
+	buw::String name;
+	SectionedSolidVector sectionedSolids;
+};
+
+OIP_NAMESPACE_OPENINFRAPLATFORM_INFRASTRUCTURE_END
+
+namespace buw
+{
+	using OpenInfraPlatform::Infrastructure::SlabFieldElement;
+}
+
+#endif // __SLABFIELDELEMENT_H_4E0E43F3630D4764A9CD2FA6214EF8A6__
---
+++ Infrastructure/src/OpenInfraPlatform/Infrastructure/SlabField/SlabFieldModel.cpp
@@ -0,0 +1,77 @@
+/*
+    Copyright (c) 2017 Technical University of Munich
+    Chair of Computational Modeling and Simulation.
+
+    TUM Open Infra Platform is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 3
+    as published by the Free Software Foundation.
+
+    TUM Open Infra Platform is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "SlabFieldModel.h"
+
+#include <algorithm>
+#include <stdexcept>
+
+OIP_NAMESPACE_OPENINFRAPLATFORM_INFRASTRUCTURE_BEGIN
+
+SlabFieldModel::SlabFieldModel()
+	: slabFieldElements_()
+{
+}
+
+SlabFieldModel::~SlabFieldModel()
+{
+}
+
+SlabFieldModel* SlabFieldModel::createFlatCopy(SlabFieldModel const& src)
+{
+	return new SlabFieldModel(src);
+}
+
+size_t SlabFieldModel::getItemCount() const
+{
+	return slabFieldElements_.size();
+}
+
+std::shared_ptr<SlabFieldElement> SlabFieldModel::getItem(size_t const index) const
+{
+	return slabFieldElements_[index];
+}
+
+std::shared_ptr<SlabFieldElement> SlabFieldModel::getItemById(size_t const id) const
+{
+	// FGI TODO
+	//for (auto const& sf : slabFields_)
+	//	if (sf->id == id) return sf;
+
+	return nullptr;
+}
+
+std::vector<std::shared_ptr<SlabFieldElement>> const& SlabFieldModel::getAllItems() const
+{
+	return slabFieldElements_;
+}
+
+void SlabFieldModel::addItem(std::shared_ptr<SlabFieldElement> const& slabFieldElement)
+{
+	slabFieldElements_.push_back(slabFieldElement);
+}
+
+void SlabFieldModel::deleteItem(std::shared_ptr<SlabFieldElement> const& slabFieldElement)
+{
+	auto const& findIt = std::find(slabFieldElements_.begin(), slabFieldElements_.end(), slabFieldElement);
+	if(findIt != slabFieldElements_.end())
+		slabFieldElements_.erase(findIt);
+	else
+		throw std::runtime_error("Deletion of slab field element failed.");
+}
+
+OIP_NAMESPACE_OPENINFRAPLATFORM_INFRASTRUCTURE_END
